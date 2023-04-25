0e635b9620df4a340bd5bd627143ba13c5a2208c
Benson Mwaura <125448975+Bensling@users.noreply.github.com>
2023-04-25 23:56:36 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -4,6 +4,7 @@
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
+ * This is a group project
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
